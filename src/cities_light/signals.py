f67e3812560002acfb345cb1a53f35c413d324b0
Muhammad Qadeer Saeed <maqadeersaeed@gmail.com>
2023-09-04 01:23:39 +0400

    Handle Cities search names

--- src/cities_light/signals.py
+++ src/cities_light/signals.py
@@ -91,7 +91,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.postgres.search import SearchVector
 from django.db import connection
-from models import AbstractCity
+# from abstract_models import AbstractCity
+from cities_light.abstract_models import AbstractCity
 
 @receiver(post_save, sender=AbstractCity)
 def update_search_vector(sender, instance, **kwargs):
